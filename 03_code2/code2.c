3d0c264d9ae3cc50b1ef65aa5c1be1dcfc2187dc
Coursera Learner <nobody@nowhere.nul>
2018-11-10 03:31:45 +0000

    Updated for loop syntax to remove ,

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -3,9 +3,9 @@ int printTriangle(int size) {
   //start with starCount being 0
   int starCount = 0;
   //count from 0 (inclusive) to size (exclusive), for each number i that you count
-  for (int i=0;,i<size,i++){
+  for (int i=0;i<size,i++){
      //count from 0 (inclusive) to i (inclusive), for each number j that you count
-    for (int j=0;,j<=i,j++){
+    for (int j=0;j<=i,j++){
         //print a "*"
       printf("*");
         //increment starCount
