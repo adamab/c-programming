69233589920800e57af0e622d3679873cf735d71
Adam A <nobody@nowhere.nul>
2018-11-18 06:06:54 +0000

    Created initial squares function, with comments for abstractions to be completed later.

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -4,13 +4,13 @@
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = max(size1, (x_offset+size2)); //Need to create a max function
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = max(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-
+  for (y=0, y<h, y++){
     //count from 0 to w. Call the number you count with x
-
+    for (x=0, x<w, x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -18,16 +18,22 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
+      if ((between(x, x_offset, x_offset+size2) & (y==y_offset | y==(y_offset+size2-1))) |//Need to create a between function
+	  (between(y, y_offset, y_offset+size2) & (x==x_offset | x==(x_offset+size2-1)))){
+	printf("*");
+      }
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+      if ((x<size1 & (y==0 | y==(size1-1))) | (y<size1 & (x==0 | x==(size1-1)))){
+	printf("#");
+      }
       //else print a space
+      else { printf(" ");}
     //when you finish counting x from 0 to w, 
     //print a newline
-
+      printf("\n");
 }
