df689450ebe3681e4f42b550181eba56288ebd62
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2018-11-22 10:23:21 -0500

    Updated syntax from commas to semi-colons in for statement, and declared variables in for loops

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -35,9 +35,9 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (y_offset + size2).  Call this h
   int h = max(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-  for (y=0, y<h, y++){
+  for (int y=0; y<h; y++){
     //count from 0 to w. Call the number you count with x
-    for (x=0, x<w, x++){
+    for (int x=0; x<w; x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
