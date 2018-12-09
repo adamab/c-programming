5499af021d1daaa4493ad86d1ada952f8fca61f4
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2018-12-09 16:57:16 -0700

    Created ranking to string function in cards source code

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -11,7 +11,27 @@ void assert_card_valid(card_t c) {
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  switc(r){
+  case STRAIGHT_FLUSH:
+    return "STRAIGHT FLUSH";
+  case FOUR_OF_A_KIND:
+    return "FOUR OF A KIND";
+  case FULL_HOUSE:
+    return "FULL HOUSE";
+  case FLUSH:
+    return "FLUSH";
+  case STRAIGHT:
+    return "STRAIGHT";
+  case THREE_OF_A_KIND:
+    return "THREE OF A KIND";
+  case TWO_PAIR:
+    return "TWO PAIR";
+  case PAIR:
+    return "PAIR";
+  case NOTHING:
+    return "NOTHING";
+  
+return "";
 }
 
 char value_letter(card_t c) {
