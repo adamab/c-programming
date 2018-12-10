f407b4a8db91c5e9fc64c7fef1abcd6ea64a47bd
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2018-12-09 20:46:03 -0700

    Making updates for explicit casting and adding parentheses

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -6,9 +6,9 @@
 
 void assert_card_valid(card_t c) {
   //Assert the value is between 2 and 14
-  assert(c.value >= 2 & c.value <= 14);
+  assert((c.value >= 2) & (c.value <= 14));
   //Assert the suit value is between 0 and 3
-  assert(c.suit >= 0 & c.suit <= 3);
+  assert((c.suit >= 0) & (c.suit <= 3));
 
 }
 
@@ -16,23 +16,23 @@ const char * ranking_to_string(hand_ranking_t r) {
   //Use switch case to return the char for hand ranking
   switch (r){
   case STRAIGHT_FLUSH:
-    return "STRAIGHT FLUSH";
+    return (char) "STRAIGHT FLUSH";
   case FOUR_OF_A_KIND:
-    return "FOUR OF A KIND";
+    return (char) "FOUR OF A KIND";
   case FULL_HOUSE:
-    return "FULL HOUSE";
+    return (char) "FULL HOUSE";
   case FLUSH:
-    return "FLUSH";
+    return (char) "FLUSH";
   case STRAIGHT:
-    return "STRAIGHT";
+    return (char) "STRAIGHT";
   case THREE_OF_A_KIND:
-    return "THREE OF A KIND";
+    return (char) "THREE OF A KIND";
   case TWO_PAIR:
-    return "TWO PAIR";
+    return (char) "TWO PAIR";
   case PAIR:
-    return "PAIR";
+    return (char) "PAIR";
   default:
-    return "NOTHING";
+    return (char) "NOTHING";
   }
 }
 
@@ -41,20 +41,22 @@ char value_letter(card_t c) {
   assert_card_valid(c);
   //Determine if it is one of the single characters to use decimal char
   if (c.value < 10) {
-    return "0" + c.value;
+    return (char) ("0" + c.value);
   } else {
     //If it is not a single digit, then use switch case to return the proper char
     switch(c.value){
     case 10:
-      return "0";
+      return (char) "0";
     case 11:
-      return "J";
+      return (char) "J";
     case 12:
-      return "Q";
+      return (char) "Q";
     case 13:
-      return "K" ;
+      return (char) "K" ;
     case 14:
-      return "A";
+      return (char) "A";
+    default:
+      return;
     }
   }
 }
@@ -66,13 +68,15 @@ char suit_letter(card_t c) {
   //Use switch case to return the char for the suit
   switch (c.suit){
   case SPADES:
-    return "s";
+    return (char) "s";
   case HEARTS:
-    return "h";
+    return (char) "h";
   case DIAMONDS:
-    return "d";
+    return (char) "d";
   case CLUBS:
-    return "c";
+    return (char) "c";
+  default:
+    return;
   }
 }
 
@@ -89,16 +93,16 @@ card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
   //Use switch case to set the proper suit value
   switch (suit_let){
-  case "SPADES":
+  case "s":
     temp.suit=0;
     break;
-  case "HEARTS":
+  case "h":
     temp.suit=1;
     break;
-  case "DIAMONDS":
+  case "d":
     temp.suit=2;
     break;
-  case "CLUBS":
+  case "c":
     temp.suit=3;
     break;
   default:
