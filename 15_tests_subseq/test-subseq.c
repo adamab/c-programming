07f40e2fc6b00b96fe08851b35d97859b7ea80bc
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-09 17:04:00 -0500

    Adding brackets to testing if statements

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -12,13 +12,13 @@ void main(){
   int test6[2] = {1,0};
   int test7[5] = {1,1,13,4,511};
 
-  if (maxSeq(test1, 7) != 5) printf("Failed for 1,2,3,4,5,4,8\n"); exit(EXIT_FAILURE);
-  if (maxSeq(test2, 9) != 3) printf("Failed for 1,2,6,5,5,4,8,7,8\n"); exit(EXIT_FAILURE);
-  if (maxSeq(test3, 4) != 2) printf("Failed for 5,3,4,4\n"); exit(EXIT_FAILURE);
-  if (maxSeq(test4, 5) != 2) printf("Failed for 3,4,4,8,7\n"); exit(EXIT_FAILURE);
-  if (maxSeq(test5, 9) != 2) printf("Failed for 4,8,6,5,4,3,2,1,0\n"); exit(EXIT_FAILURE);
-  if (maxSeq(test6, 2) != 1) printf("Failed for 1,0\n"); exit(EXIT_FAILURE);
-  if (maxSeq(test7, 5) != 2) printf("Failed for 1,1,13,4,511\n"); exit(EXIT_FAILURE);
+  if (maxSeq(test1, 7) != 5) {printf("Failed for 1,2,3,4,5,4,8\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test2, 9) != 3) {printf("Failed for 1,2,6,5,5,4,8,7,8\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test3, 4) != 2) {printf("Failed for 5,3,4,4\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test4, 5) != 2) {printf("Failed for 3,4,4,8,7\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test5, 9) != 2) {printf("Failed for 4,8,6,5,4,3,2,1,0\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test6, 2) != 1) {printf("Failed for 1,0\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test7, 5) != 2) {printf("Failed for 1,1,13,4,511\n"); exit(EXIT_FAILURE);}
 
   exit(EXIT_SUCCESS);
 }
