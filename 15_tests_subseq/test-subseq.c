870a3d6485171cc22e91573d286704f318b7d1f9
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-09 10:31:21 -0500

    updating syntax of test script

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -3,14 +3,14 @@
 
 size_t maxSeq(int * array, size_t n);
 
-void main(){
-  test1[7] = {1,2,3,4,5,4,8}
-  test2[9] = {1,2,6,5,5,4,8,7,8}
-  test3[4] = {5,3,4,4}
-  test4[5] = {3,4,4,8,7}
-  test5[9] = {4,8,6,5,4,3,2,1,0}
-  test6[2] = {1,0}
-  test7[5] = {1,1,13,4,511}
+int main(){
+  int test1[7] = {1,2,3,4,5,4,8};
+  int test2[9] = {1,2,6,5,5,4,8,7,8};
+  int test3[4] = {5,3,4,4};
+  int test4[5] = {3,4,4,8,7};
+  int test5[9] = {4,8,6,5,4,3,2,1,0};
+  int test6[2] = {1,0};
+  int test7[5] = {1,1,13,4,511};
 
   if (maxSeq(test1, 7) != 5) printf("Failed for 1,2,3,4,5,4,8\n"); exit(EXIT_FAILURE);
   if (maxSeq(test2, 9) != 3) printf("Failed for 1,2,6,5,5,4,8,7,8\n"); exit(EXIT_FAILURE);
