b4badf31f89c525d0143c3668f57e2114e346c29
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-09 17:15:30 -0500

    Updating with a new test case

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -11,6 +11,7 @@ void main(){
   int test5[9] = {4,8,6,5,4,3,2,1,0};
   int test6[2] = {1,0};
   int test7[5] = {1,1,13,4,511};
+  int test8[5] = {10,9,7,2,0};
 
   if (maxSeq(test1, 7) != 5) {printf("Failed for 1,2,3,4,5,4,8\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test2, 9) != 3) {printf("Failed for 1,2,6,5,5,4,8,7,8\n"); exit(EXIT_FAILURE);}
@@ -19,6 +20,7 @@ void main(){
   if (maxSeq(test5, 9) != 2) {printf("Failed for 4,8,6,5,4,3,2,1,0\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test6, 2) != 1) {printf("Failed for 1,0\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test7, 5) != 2) {printf("Failed for 1,1,13,4,511\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test8, 5) != 1) {printf("Failed for 10,9,7,2,0\n"); exit(EXIT_FAILURE);}
 
   exit(EXIT_SUCCESS);
 }
