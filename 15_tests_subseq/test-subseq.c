581e1af0764d950e1d7fe8013ed0f44517b6f447
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-09 17:38:37 -0500

    Updating test case

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -9,7 +9,7 @@ void main(){
   int test3[4] = {5,3,4,4};
   int test4[5] = {3,4,4,8,7};
   int test5[9] = {4,8,6,5,4,3,2,1,0};
-  int test6[2] = {1,0};
+  int test6[] = {};
   int test7[5] = {1,1,13,4,511};
   int test8[5] = {-2,-1,0,1,2};
   int test9[1] = {0};
@@ -20,7 +20,7 @@ void main(){
   if (maxSeq(test3, 4) != 2) {printf("Failed for 5,3,4,4\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test4, 5) != 2) {printf("Failed for 3,4,4,8,7\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test5, 9) != 2) {printf("Failed for 4,8,6,5,4,3,2,1,0\n"); exit(EXIT_FAILURE);}
-  if (maxSeq(test6, 2) != 1) {printf("Failed for 1,0\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test6, 0) != 0) {printf("Failed for \n"); exit(EXIT_FAILURE);}
   if (maxSeq(test7, 5) != 2) {printf("Failed for 1,1,13,4,511\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test8, 5) != 5) {printf("Failed for -2,-1,0,1,2\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test9, 1) != 1) {printf("Failed for 0\n"); exit(EXIT_FAILURE);}
