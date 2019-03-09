15e8547199597c54eb8e0de9b53f4537e28b1121
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-09 17:20:43 -0500

    Updating tests with correct count of increasing

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -18,9 +18,9 @@ void main(){
   if (maxSeq(test3, 4) != 2) {printf("Failed for 5,3,4,4\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test4, 5) != 2) {printf("Failed for 3,4,4,8,7\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test5, 9) != 2) {printf("Failed for 4,8,6,5,4,3,2,1,0\n"); exit(EXIT_FAILURE);}
-  if (maxSeq(test6, 2) != 1) {printf("Failed for 1,0\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test6, 2) != 0) {printf("Failed for 1,0\n"); exit(EXIT_FAILURE);}
   if (maxSeq(test7, 5) != 2) {printf("Failed for 1,1,13,4,511\n"); exit(EXIT_FAILURE);}
-  if (maxSeq(test8, 5) != 1) {printf("Failed for 11,9,7,2,0\n"); exit(EXIT_FAILURE);}
+  if (maxSeq(test8, 5) != 0) {printf("Failed for 11,9,7,2,0\n"); exit(EXIT_FAILURE);}
 
   exit(EXIT_SUCCESS);
 }
