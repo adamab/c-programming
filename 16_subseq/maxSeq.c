195abd92f0626e8d24c737b3fa43618c95962466
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-10 08:50:26 -0400

    Create source code and Makefile, copy test script

---
+++ 16_subseq/maxSeq.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n){
+  //Check if size n is less than 0, return -1 if so
+  if(n < 0) return -1;
+  //Check if size n is 0, return 0 if so
+  if(n == 0) return 0;
+  //Create variable count initialized to 1
+  int count = 1;
+  //Create variable maxCount initialized to 1
+  int maxCount = 1;
+  //Loop from 1 to n(exclusive)
+  for(i = 1; i < n; i++){
+  //Check if value at index of current step is greater than the value at index of the previous step
+  //Add 1 to count if the above holds
+    if(array[i] > array[i-1]) count+=1;
+    //Otherwise, count = 1
+    else count = 1;
+  //Check if value of count is greater than maxCount
+  //Set the value of maxCount to the value of count if the above holds
+    if(count > maxCount) maxCount = count;
+  }
+  //Return the value of maxCount
+  return maxCount;
+}
