bc977161b02182c0c481ecd7ac5309ef4dc47e60
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-10 19:38:19 -0600

    Updating test and source code to properly handle negative n

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -5,7 +5,7 @@ size_t maxSeq(int * array, size_t n){
   //Check if size n is less than 0, return -1 if so
   //if(n < 0) return -1;
   //Check if size n is 0, return 0 if so
-  if(n == 0) return 0;
+  if(n <= 0) return 0;
   //Create variable count initialized to 1
   int count = 1;
   //Create variable maxCount initialized to 1
