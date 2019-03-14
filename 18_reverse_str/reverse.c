1b2adb184db7cb14deb36b5ba4c3641d67bf0d90
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-13 20:54:05 -0600

    Including string pointer for counting

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,16 +3,16 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //Dereference str and store it in strcpy
-
+  //Initialize copy pointer
+  char * strptr = str;
   //Create the count integer n
   int n = 1;
   //Loop through str until reaching \0
-  while (*str != '\0'){
+  while (*strptr != '\0'){
   //Increase n by 1
     n++;
   //Increase str pointer by 1
-    str++;
+    strptr++;
   }
   //Create strcpy with the size of n
   char stringcpy[n+1];
