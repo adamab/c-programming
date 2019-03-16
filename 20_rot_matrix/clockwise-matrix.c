1dfdb32de369d801e0c85eda5e598953bb03d68a
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-15 20:33:07 -0600

    Creating Makefile and rotation function

---
+++ 20_rot_matrix/clockwise-matrix.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+void rotate(char matrix[10][10]) {
+  //Check for NULL case and return
+  if (matrix == NULL) return;
+  //Initialize temporary matrix
+  char tmpMatrix[10][10] = matrix;
+  //Loop i from 0 to 9
+  for (int i = 0; i < 10; i++){
+    //Loop j from 0 to 9
+    for (int j = 0; j < 10; j++){
+      //Set matrix[j][9-i] to equal tmpMatrix[i][j]
+      matrix[j][9-i] = tmpMatrix[i][j];
+    }
+  }
+}
