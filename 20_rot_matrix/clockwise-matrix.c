64e21d11c6ecb4c5536da11adee73131eb363a7b
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-15 20:47:17 -0600

    Updating initialization of temporary matrix

--- 20_rot_matrix/clockwise-matrix.c
+++ 20_rot_matrix/clockwise-matrix.c
@@ -4,7 +4,15 @@ void rotate(char matrix[10][10]) {
   //Check for NULL case and return
   if (matrix == NULL) return;
   //Initialize temporary matrix
-  char tmpMatrix[10][10] = matrix;
+  char tmpMatrix[10][10];
+  //Loop i from 0 to 9
+  for (int i = 0; i < 10; i++){
+    //Loop j from 0 to 9
+    for (int j = 0; j < 10; j++){
+      //Set tmpMatrix[i][j] to equal matrix[i][j]
+      tmpMatrix[i][j] = matrix[i][j];
+    }
+  }
   //Loop i from 0 to 9
   for (int i = 0; i < 10; i++){
     //Loop j from 0 to 9
