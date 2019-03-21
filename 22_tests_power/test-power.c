ab3b7aa40aed6d357d0c12a8b210779e285c63ee
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-20 20:43:37 -0600

    Updating print statement to use double quotation:

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -6,7 +6,7 @@ unsigned power(unsigned x, unsigned y);
 void run_check(unsigned x, unsigned y, unsigned expected_ans){
 
   if (power(x, y) == expected_ans) return;
-  printf('Failed for x=%d, y=%d, expected=%d\n',x,y,expected_ans);
+  printf("Failed for x=%d, y=%d, expected=%d\n",x,y,expected_ans);
   exit(EXIT_FAILURE);
 }
 
@@ -17,6 +17,7 @@ int main(){
   run_check(0,3,0);
   run_check(3,0,1);
   run_check(4294967295,1,4294967295);
+  printf("All tests passed");
   return EXIT_SUCCESS;
 
 }
