cfe46d267abe320ab53ce40bbc09dfeed52754b8
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-31 08:08:23 -0600

    Updating pointer references

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -4,28 +4,28 @@
 #include "deck.h"
 void print_hand(deck_t * hand){
   //Loop thorugh the length of the hand  
-  for(int i = 0; i < hand->n_cards; i++){  
+  for(int i = 0; i < *hand->n_cards; i++){  
     //Print the card at index i
-    print_card(*hand.cards[i]);
+    print_card(**hand.cards[i]);
   }
 }
 
 int deck_contains(deck_t * d, card_t c) {
   //Loop through the length of the deck
-  for(int i = 0; i < d->n_cards; i++){
+  for(int i = 0; i < *d->n_cards; i++){
     //Check if the card is equal to the check for card
-    if(*d.cards[i] == c) return 1;
+    if(**d.cards[i] == c) return 1;
   }
   return 0;
 }
 
 void shuffle(deck_t * d){
   //Save the cards addresses in a new array of cards
-  card_t * cards[d->n_cards] = *d.cards;
+  card_t * cards[*d->n_cards] = *d.cards;
   //Loop through the length of the cards and assign them to a random position in the deck
-  for(int i = 0; i < d->n_cards; i++){
+  for(int i = 0; i < *d->n_cards; i++){
     //Assign card addresses to a random position in the original deck
-    d->cards[rand()%52] = cards[i];
+    *d->cards[rand()%52] = cards[i];
   }
 }
 
