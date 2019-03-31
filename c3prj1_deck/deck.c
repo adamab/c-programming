77f131f35b12d21f7853c21b7463e96b563c7670
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-03-31 08:10:19 -0600

    Updating pointer reference for non pointer

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -4,7 +4,7 @@
 #include "deck.h"
 void print_hand(deck_t * hand){
   //Loop thorugh the length of the hand  
-  for(int i = 0; i < *hand->n_cards; i++){  
+  for(int i = 0; i < hand->n_cards; i++){  
     //Print the card at index i
     print_card(**hand.cards[i]);
   }
@@ -12,7 +12,7 @@ void print_hand(deck_t * hand){
 
 int deck_contains(deck_t * d, card_t c) {
   //Loop through the length of the deck
-  for(int i = 0; i < *d->n_cards; i++){
+  for(int i = 0; i < d->n_cards; i++){
     //Check if the card is equal to the check for card
     if(**d.cards[i] == c) return 1;
   }
@@ -21,9 +21,9 @@ int deck_contains(deck_t * d, card_t c) {
 
 void shuffle(deck_t * d){
   //Save the cards addresses in a new array of cards
-  card_t * cards[*d->n_cards] = *d.cards;
+  card_t * cards[d->n_cards] = *d.cards;
   //Loop through the length of the cards and assign them to a random position in the deck
-  for(int i = 0; i < *d->n_cards; i++){
+  for(int i = 0; i < d->n_cards; i++){
     //Assign card addresses to a random position in the original deck
     *d->cards[rand()%52] = cards[i];
   }
