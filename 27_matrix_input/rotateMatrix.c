dc26a64cfcc265b44d5c5f927c4e268fa13d93b7
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-04-28 14:54:22 -0600

    Updating matrix printing functionality to use character format string

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -32,7 +32,7 @@ void printMatrix(char matrix[10][10]) {
   }
   for (int i = 0; i < 10; i++){
     for (int j = 0; j < 10; j++){
-      printf(&matrix[i][j]);
+      printf("%c",matrix[i][j]);
     }
     printf("\n");
   }
