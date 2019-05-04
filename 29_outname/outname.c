249cddafe65f069258bdf9ce46224a69c5d0f6bd
Adam A <nobody@nowhere.nul>
2019-05-04 22:26:05 +0000

    Creating function to name output file

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,9 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  //outfileNAme is inputName + ".counts\0", so add 8 to its length.
+  char * outFileName = malloc((strlen(inputName) + 8) * sizeof(*outFileName));
+  strncpy(outFileName, inputName, strlen(inputName)+1);
+  strncat(outFileName, ".counts", 7);
+  return outFileName;
 }
