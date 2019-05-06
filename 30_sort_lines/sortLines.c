ec46ccdc80d1e3f542fb6b0a25bf1d7ef65bca8b
Adam Abdulhafid <abdul.hyphen.hafid@gmail.com>
2019-05-05 18:38:07 -0600

    Updating sort files to close file

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -63,6 +63,10 @@ void sortFiles(char ** argv, int argc){
   }
   free(lineArray);
   printf("All files sorted!");
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char ** argv) {
